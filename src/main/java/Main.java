0ce35cf1c960a3c5f252aee5ec4e3b53748e3d82
Roy Ash <roy.ash456@gmail.com>
2020-06-13 22:04:16 +0300

    cycle, schema, configuration works

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,11 @@
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import dataLayer.crud.Query;
-import dataLayer.crud.filters.Eq;
+import dataLayer.crud.Entity;
 import dataLayer.readers.Reader;
 
-import dataLayer.crud.Entity;
-
 import java.io.IOException;
-import java.util.*;
+import java.util.Calendar;
+import java.util.Set;
 
 import static dataLayer.crud.Query.create;
 import static dataLayer.crud.Query.read;
@@ -27,80 +25,88 @@ public class Main
 
 		Calendar calendar = Calendar.getInstance();
 		Calendar calendar2 = Calendar.getInstance();
-		calendar.set(1999,Calendar.MAY,16);
-		Entity movie = Entity.of("Movie", new HashMap<String, Object>(Map.of("ID", "111111",
-				"releaseDate", calendar.getTimeInMillis(),
-				"title", "Star Wars: Episode I – The Phantom Menace",
-				"avgRating", 4.5,
-				"length", 210,
-				"media", "Blu-Ray",
-				"income", 10000000)));
+		calendar.set(1999, Calendar.MAY, 16);
+		Entity movie = Entity.of("Movie")
+				.putField("ID", "111111")
+				.putField("releaseDate", calendar.getTimeInMillis())
+				.putField("title", "Star Wars: Episode I – The Phantom Menace")
+				.putField("avgRating", 4.5)
+				.putField("length", 210)
+				.putField("media", "Blu-Ray")
+				.putField("income", 10000000);
 
-		calendar.set(1944, Calendar.MAY,14);
-		calendar2.set(2020,Calendar.OCTOBER,10);
-		Entity director = Entity.of("User", new HashMap<String, Object>(Map.of("username", "curiousGeorge",
-				"password", "lucasFilms123",
-				"email", "george@gmail.com",
-				"lastLogin", calendar2.getTimeInMillis(),
-				"ID", 121212,
-				"name", "George Lucas",
-				"dateofbirth", calendar.getTimeInMillis(),
-				"gender", true,
-				"moviesDirector", Set.of(movie))));
+		calendar.set(1944, Calendar.MAY, 14);
+		calendar2.set(2020, Calendar.OCTOBER, 10);
+		Entity director = Entity.of("User")
+				.putField("username", "curiousGeorge")
+				.putField("password", "lucasFilms123")
+				.putField("email", "george@gmail.com")
+				.putField("lastLogin", calendar2.getTimeInMillis())
+				.putField("ID", 121212)
+				.putField("name", "George Lucas")
+				.putField("dateofbirth", calendar.getTimeInMillis())
+				.putField("gender", true)
+				.putField("moviesDirector", Set.of(movie));
 
-		calendar.set(1954, Calendar.AUGUST,22);
-		calendar2.set(2020,Calendar.OCTOBER,10);
-		Entity producer = Entity.of("User", new HashMap<String, Object>(Map.of("username", "richieRich",
-				"password", "richard789",
-				"email", "richard@gmail.com",
-				"lastLogin", calendar2.getTimeInMillis(),
-				"ID", 131313,
-				"name", "Richard McCallum",
-				"dateofbirth", calendar.getTimeInMillis(),
-				"gender", true,
-				"moviesProducer", Set.of(movie))));
+		calendar.set(1954, Calendar.AUGUST, 22);
+		calendar2.set(2020, Calendar.OCTOBER, 10);
+		Entity producer = Entity.of("User")
+				.putField("username", "richieRich")
+				.putField("password", "richard789")
+				.putField("email", "richard@gmail.com")
+				.putField("lastLogin", calendar2.getTimeInMillis())
+				.putField("ID", 131313)
+				.putField("name", "Richard McCallum")
+				.putField("dateofbirth", calendar.getTimeInMillis())
+				.putField("gender", true)
+				.putField("moviesProducer", Set.of(movie));
 
-		calendar.set(1989, Calendar.MARCH,5);
-		calendar2.set(2020,Calendar.OCTOBER,10);
-		Entity actor1 = Entity.of("User", new HashMap<String, Object>(Map.of("username", "jakeWalker",
-				"password", "jakeJ231",
-				"email", "jake@gmail.com",
-				"lastLogin", calendar2.getTimeInMillis(),
-				"ID", 141414,
-				"name", "Jake Lloyd",
-				"dateofbirth", calendar.getTimeInMillis(),
-				"gender", true,
-				"moviesActor", Set.of(movie))));
+		calendar.set(1989, Calendar.MARCH, 5);
+		calendar2.set(2020, Calendar.OCTOBER, 10);
+		Entity actor1 = Entity.of("User")
+				.putField("username", "jakeWalker")
+				.putField("password", "jakeJ231")
+				.putField("email", "jake@gmail.com")
+				.putField("lastLogin", calendar2.getTimeInMillis())
+				.putField("ID", 141414)
+				.putField("name", "Jake Lloyd")
+				.putField("dateofbirth", calendar.getTimeInMillis())
+				.putField("gender", true)
+				.putField("moviesActor", Set.of(movie));
 
-		calendar.set(1944, Calendar.MAY,25);
-		calendar2.set(2020,Calendar.OCTOBER,10);
-		Entity actor2 = Entity.of("User", new HashMap<String, Object>(Map.of("username", "wizOz",
-				"password", "wizardOz212",
-				"email", "frank@gmail.com",
-				"lastLogin", calendar2.getTimeInMillis(),
-				"ID", 151515,
-				"name", "Frank Oz",
-				"dateofbirth", calendar.getTimeInMillis(),
-				"gender", true,
-				"moviesActor", Set.of(movie))));
+		calendar.set(1944, Calendar.MAY, 25);
+		calendar2.set(2020, Calendar.OCTOBER, 10);
+		Entity actor2 = Entity.of("User")
+				.putField("username", "wizOz")
+				.putField("password", "wizardOz212")
+				.putField("email", "frank@gmail.com")
+				.putField("lastLogin", calendar2.getTimeInMillis())
+				.putField("ID", 151515)
+				.putField("name", "Frank Oz")
+				.putField("dateofbirth", calendar.getTimeInMillis())
+				.putField("gender", true)
+				.putField("moviesActor", Set.of(movie));
 
-		calendar.set(1944, Calendar.AUGUST,11);
-		calendar2.set(2020,Calendar.OCTOBER,10);
-		Entity actor3 = Entity.of("User", new HashMap<String, Object>(Map.of("username", "mcDiarmid",
-				"password", "ianMac547",
-				"email", "ian@gmail.com",
-				"lastLogin", calendar2.getTimeInMillis(),
-				"ID", 161616,
-				"name", "Ian McDiarmid",
-				"dateofbirth", calendar.getTimeInMillis(),
-				"gender", true,
-				"moviesActor", Set.of(movie))));
+		calendar.set(1944, Calendar.AUGUST, 11);
+		calendar2.set(2020, Calendar.OCTOBER, 10);
+		Entity actor3 = Entity.of("User")
+				.putField("username", "mcDiarmid")
+				.putField("password", "ianMac547")
+				.putField("email", "ian@gmail.com")
+				.putField("lastLogin", calendar2.getTimeInMillis())
+				.putField("ID", 161616)
+				.putField("name", "Ian McDiarmid")
+				.putField("dateofbirth", calendar.getTimeInMillis())
+				.putField("gender", true)
+				.putField("moviesActor", Set.of(movie));
 
-		Entity genre1 = Entity.of("Genre", Map.of("name", "Sci-Fi",
-			"movies", Set.of(movie)));
+		Entity genre1 = Entity.of("Genre")
+				.putField("name", "Sci-Fi")
+				.putField("movies", Set.of(movie));
 
-		Entity genre2 = Entity.of("Genre", Map.of("name", "Adventure",
-				"movies", Set.of(movie)));
+		Entity genre2 = Entity.of("Genre")
+				.putField("name", "Adventure")
+				.putField("movies", Set.of(movie));
 
 		movie.putField("directors", Set.of(director));
 
@@ -108,20 +114,24 @@ public class Main
 
 		movie.putField("genres", Set.of(genre1, genre2));
 
-		Entity role1 = Entity.of("MovieRole", Map.of("user", actor1,
-				"movie", movie,
-				"roles", Set.of("Anakin Skywalker")));
+		Entity role1 = Entity.of("MovieRole")
+				.putField("user", actor1)
+				.putField("movie", movie)
+				.putField("roles", Set.of("Anakin Skywalker"));
 
-		Entity role2 = Entity.of("MovieRole", Map.of("user", actor2,
-				"movie", movie,
-				"roles", Set.of("Yoda")));
+		Entity role2 = Entity.of("MovieRole")
+				.putField("user", actor2)
+				.putField("movie", movie)
+				.putField("roles", Set.of("Yoda"));
 
-		Entity role3 = Entity.of("MovieRole", Map.of("user", actor3,
-				"movie", movie,
-				"roles", Set.of("Sheev Palpatine")));
+		Entity role3 = Entity.of("MovieRole")
+				.putField("user", actor3)
+				.putField("movie", movie)
+				.putField("roles", Set.of("Sheev Palpatine"));
 
 		create(movie, director, producer, actor1, actor2, actor3, genre1, genre2, role1, role2, role3);
 
+		//noinspection OptionalGetWithoutIsPresent
 		Entity result = read(eq("User", "ID", 121212)).stream()
 				.findFirst()
 				.get();
